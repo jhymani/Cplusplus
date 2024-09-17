3e0b67c194e9947d273436bdde3a1b61f26ca4d4
jhymani <jajoseph0624@my.msutexas.edu>
2024-09-17 15:39:01 -0500

    main.cpp

--- AssignmentsFall/P01/main.cpp
+++ AssignmentsFall/P01/main.cpp
@@ -7,8 +7,11 @@ using namespace std;
 
 int main() {
     string f1, op, f2;
+    int iteration = 0; // Counter for the number of iterations
 
-    while (cin >> f1 >> op >> f2) {
+    cout << "Enter fractions and operator in the form of a/b operator c/d" << endl;
+
+    while (iteration < 4 && cin >> f1 >> op >> f2) {
         Fraction F1(f1);
         Fraction F2(f2);
 
@@ -19,13 +22,25 @@ int main() {
         } else if (op == "*") {
             cout << F1 << " * " << F2 << " = " << F1 * F2 << endl;
         } else if (op == "/") {
-            cout << F1 << " / " << F2 << " = " << F1 / F2 << endl;
+            if (F2.getNumerator() == 0) {
+                cout << "Error: Division by zero." << endl;
+            } else {
+                cout << F1 << " / " << F2 << " = " << F1 / F2 << endl;
+            }
         } else if (op == "==") {
             cout << F1 << " == " << F2 << " = " << (F1 == F2 ? "True" : "False") << endl;
         } else {
             cout << "Invalid operator" << endl;
         }
+
+        iteration++; // Increment the iteration counter
+
+        if (iteration < 4) {
+            cout << "Enter 2 fractions and operator in the middle:" << endl;
+        }
     }
+
+    cout << "Program has completed after 4 iterations." << endl;
 
     return 0;
 }
